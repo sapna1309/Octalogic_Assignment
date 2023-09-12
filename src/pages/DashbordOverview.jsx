93a93c313dc55c6ecc0d767e7f903662e4f9223b
Sapna Sharma <sapna.sharma220516@gmail.com>
2023-09-12 23:41:47 +0530

    basic structure on overview page added for left hand side

--- src/pages/DashbordOverview.jsx
+++ src/pages/DashbordOverview.jsx
@@ -2,8 +2,20 @@
 
 const DashbordOverview = () => {
   return (
-    <div>DashbordOverview</div>
-  )
-}
+    <div className="bg-bg_gray w-[1380px] h-[1024px] felx ">
+      <div className="w-[96px] h-[1024px] border-r-1 border-[#D1D5DB] bg-[#FFFFFF] py-[10px] px-[12px] flex flex-col gap-[48px]">
+        <div className="w-[48px] h-[48px]"></div>
+        <div className="w-[72px] h-[116px] flex flex-col gap-[16px]">
+          <div className="w-[72px] h-[50px] rounded-[6px] py-[5px] px-[0px]"></div>
+          <div className="w-[72px] h-[50px] rounded-[6px] py-[5px] px-[4px]"></div>
+        </div>
+        <div className="w-[72px] h-[744px] flex flex-col gap-[16px]">
+          <div className="w-[72px] h-[50px] rounded-[6px] py-[5px] px-[0px]"></div>
+        </div>
+      </div>
+      <div></div>
+    </div>
+  );
+};
 
-export default DashbordOverview
+export default DashbordOverview;
